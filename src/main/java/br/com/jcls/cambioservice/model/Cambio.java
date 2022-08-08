61821a31ff1d6a7a6207f568dc672e39d70e5c47
Juliano C L Santos <foxports@hotmail.com>
2022-08-08 00:36:20 -0300

    Lombok implements

--- src/main/java/br/com/jcls/cambioservice/model/Cambio.java
+++ src/main/java/br/com/jcls/cambioservice/model/Cambio.java
@@ -1,11 +1,17 @@
 package br.com.jcls.cambioservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "cambio")
 public class Cambio implements Serializable {
 
@@ -31,75 +37,4 @@ public class Cambio implements Serializable {
     @Transient
     private String environment;
 
-    public Cambio() {}
-
-    public Cambio(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue, String environment) {
-        this.id = id;
-        this.from = from;
-        this.to = to;
-        this.conversionFactor = conversionFactor;
-        this.convertedValue = convertedValue;
-        this.environment = environment;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public BigDecimal getConversionFactor() {
-        return conversionFactor;
-    }
-
-    public void setConversionFactor(BigDecimal conversionFactor) {
-        this.conversionFactor = conversionFactor;
-    }
-
-    public BigDecimal getConvertedValue() {
-        return convertedValue;
-    }
-
-    public void setConvertedValue(BigDecimal convertedValue) {
-        this.convertedValue = convertedValue;
-    }
-
-    public String getEnvironment() {
-        return environment;
-    }
-
-    public void setEnvironment(String environment) {
-        this.environment = environment;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cambio cambio = (Cambio) o;
-        return Objects.equals(id, cambio.id) && Objects.equals(from, cambio.from) && Objects.equals(to, cambio.to) && Objects.equals(conversionFactor, cambio.conversionFactor) && Objects.equals(convertedValue, cambio.convertedValue) && Objects.equals(environment, cambio.environment);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, from, to, conversionFactor, convertedValue, environment);
-    }
 }

b4fec815f6a48e041939e0f62c2251ce1fe79611
Juliano C L Santos <foxports@hotmail.com>
2022-09-12 00:26:23 -0300

    Basic test spring boot kafka - callback method

--- src/main/java/br/com/jcls/cambioservice/service/TopicProducer.java
+++ src/main/java/br/com/jcls/cambioservice/service/TopicProducer.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.Properties;
 
@@ -15,9 +18,27 @@ public class TopicProducer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void send(String message){
+    public void send(String message) {
+        String topicName = "topico.comando.teste";
+
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
+
         log.info("Payload enviado: {}", message);
-        kafkaTemplate.send("topico.comando.teste", message);
+        kafkaTemplate.send(topicName, message);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Unable to send message=["
+                        + message + "] due to : " + ex.getMessage());
+            }
+        });
     }
 
 }
